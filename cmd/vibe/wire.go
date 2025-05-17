edf48dc97974311de565ecc1d19393d0eb76c484
Howard Yeh <hayeah@gmail.com>
2025-05-17 17:16:44 +0800

    generate wire

--- cmd/vibe/wire.go
+++ cmd/vibe/wire.go
@@ -2,19 +2,25 @@
 
 package main
 
-import "github.com/google/wire"
+import (
+	"github.com/google/wire"
+	"github.com/hayeah/fork2/render"
+)
 
 func BuildOutPipeline(root string, args OutCmd) (*OutPipeline, error) {
-    wire.Build(
-        ProvideCounter,
-        ProvideMetrics,
-        ProvideFSList,
-        ProvideResolver,
-        ProvideRenderer,
-        ProvideDirectoryTreeService,
-        ProvideFileMapService,
-        ProvideContentLoader,
-        wire.Struct(new(OutPipeline), "DT", "Renderer", "FileMap", "Metrics", "Loader"),
-    )
-    return nil, nil
+	wire.Build(
+		ProvideCounter,
+		ProvideMetrics,
+		ProvideFSList,
+		ProvideResolver,
+		ProvideRenderer,
+		wire.Bind(new(RendererService), new(*render.Renderer)),
+		ProvideDirectoryTreeService,
+		wire.Bind(new(DirectoryTreeService), new(*DirectoryTree)),
+		ProvideFileMapService,
+		wire.Bind(new(FileMapService), new(*FileMapWriter)),
+		ProvideContentLoader,
+		wire.Struct(new(OutPipeline), "DT", "Renderer", "FileMap", "Metrics", "Loader"),
+	)
+	return nil, nil
 }
